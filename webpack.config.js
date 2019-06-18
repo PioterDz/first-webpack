bbaf40a9a918ffb8e935f1a8ff08d1f0fa8c08f2
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-18 19:06:27 +0200

    add babel-loader with env and react presets

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: './node_modules'
+            }
+        ]
     }
 };
